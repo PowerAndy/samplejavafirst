58db2151f4d55d0a9d4359a18dfb722e8fac977e
Admin <Admin@Admin-PC.lan>
2017-09-22 18:40:15 +0800

    1、项目结构初次提交

---
+++ src/com/alibaba/tao/TestJava.java
@@ -0,0 +1,22 @@
+/**   
+* @Title: TestJava.java 
+* @Package com.alibaba.tao 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author （作者）  
+* @date 2017年9月22日 下午6:26:31 
+* @version V1.0   
+*/
+package com.alibaba.tao;
+
+/**
+ * @ClassName: TestJava
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author (作者)
+ * @date 2017年9月22日 下午6:26:31
+ * @version V1.0
+ */
+public class TestJava {
+	public void printHello() {
+		System.out.println("你好，Github");
+	}
+}
